6815d5dec95cbae16cb17bf95654372aa2497659
kritak <schmuck123@hotmail.com>
2016-02-09 07:57:51 +0100

    Create settings.py

---
+++ lib/settings.py
@@ -0,0 +1,10 @@
+#modder settings
+
+
+
+#chance of slain monster to drop item
+spoils_of_war_chance= 0.15 
+# parrychance to block attack - longer weaponrange of defender
+parrychance= 0.33
+# set true for cheatmode!
+instakill = False
